a37f1081b25e2fcdcd34a48a0f06d7d75e32d44c
Danishdnial <116070883+Danishdnial@users.noreply.github.com>
2022-11-08 14:29:38 +0800

    Add files via upload

---
+++ exerciseweek4.py
@@ -0,0 +1,8 @@
+a= int (input('Please key in first integer:'))
+b= int (input('Please key in second integer:'))
+first_int = a
+second_int = b
+if first_int>second_int:
+    print ("The first int is bigger!")
+else:
+    print ("The second int is bigger!")
